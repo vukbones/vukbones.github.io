ac7b14ba762b5eed4a597375652d2048d4f99a13
vukbones <vucossaa@gmail.com>
2023-02-13 17:06:10 +0100

    Added function game to play 5 games and fixed roPaScRound func

--- script.js
+++ script.js
@@ -11,35 +11,60 @@ function getComputerChoice () {
     }
 }
 
-let userChoice = prompt("Rock, paper or scissors? ").toLowerCase()
-
 function roPaScRound(playerSelection, computerSelection){
-    if (playerSelection === "rock" && computerSelection === "paper"){
-        return "Computer wins!";
+    if (playerSelection === "rock" && computerSelection === "paper"){        
+        return 0;
     }
     else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "Player wins!";
+        return 1;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return "Computer wins!";
+        return 0;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "Player wins!";
+        return 1;
     }
-    else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "Player wins!";
+    else if (playerSelection === "scissors" && computerSelection === "paper"){   
+        return 1;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors"){
-        return "Computer wins!"
+        return 0;
     }
     else if (playerSelection === computerSelection){
-        return "It's a draw!"
+        return 2;
     }
 }
 
-let computer = getComputerChoice();
 
-console.log(`You chose ${userChoice}`)
-console.log(`Computer chose ${computer}`)
+function game(){
+    let numPlayerWon = 0;
+    let numComputerWon = 0;
+    for (let i = 0; i < 5; i++){
+        let userChoice = prompt("Rock, paper or scissors? ").toLowerCase();
+        let computer = getComputerChoice();
+        console.log(`You chose ${userChoice}`);
+        console.log(`Computer chose ${computer}`);
+        if (roPaScRound(userChoice, computer) === 1){
+            console.log("Player wins this one match!")
+            numPlayerWon++;
+        }
+        else if(roPaScRound(userChoice, computer) === 0){
+            console.log("Computer wins this one match!");
+            numComputerWon++;
+        }
+        else{
+            console.log("Draw match!");
+        }
+    }
+    if (numComputerWon > numPlayerWon){
+        return "Computer won!"
+    }
+    else if(numComputerWon < numPlayerWon){
+        return "Player won!"
+    }
+    else{
+        return "Tie game!"
+    }
+}
 
-console.log(roPaScRound(userChoice, computer))
+console.log(game());
