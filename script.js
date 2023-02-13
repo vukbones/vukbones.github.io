3efb5d6d4fe07ccb475ad030da813c1897d189c4
vukbones <vucossaa@gmail.com>
2023-02-13 14:11:32 +0100

    Added player choice and function to make a round

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let computer = function getComputerChoice () {
+function getComputerChoice () {
     computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     if (computerChoice === 1){
         return "rock";
@@ -11,4 +11,35 @@ let computer = function getComputerChoice () {
     }
 }
 
-console.log(computer());
+let userChoice = prompt("Rock, paper or scissors? ").toLowerCase()
+
+function roPaScRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "paper"){
+        return "Computer wins!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "Player wins!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "Computer wins!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "Player wins!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "Player wins!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "Computer wins!"
+    }
+    else if (playerSelection === computerSelection){
+        return "It's a draw!"
+    }
+}
+
+let computer = getComputerChoice();
+
+console.log(`You chose ${userChoice}`)
+console.log(`Computer chose ${computer}`)
+
+console.log(roPaScRound(userChoice, computer))
